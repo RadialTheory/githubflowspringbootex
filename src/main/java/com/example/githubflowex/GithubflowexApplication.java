355429fb8aef8eecb43e0b1839b0711cd8494408
vjindalradialtheory <vikas.jindal@radialtheory.com>
2022-09-30 10:47:38 +0530

    Adding initial structure

---
+++ src/main/java/com/example/githubflowex/GithubflowexApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubflowex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubflowexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubflowexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubflowex/GithubflowexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubflowex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubflowexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
